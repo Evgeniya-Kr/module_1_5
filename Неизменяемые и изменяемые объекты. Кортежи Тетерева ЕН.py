ebc895b8aa1e911f2f9048ae2b856b1d2cc8d173
Evgeniya-Kr <jenya-kr@yandex.ru>
2024-08-16 01:08:21 +0700

    Add files via upload

--- Неизменяемые и изменяемые объекты. Кортежи Тетерева ЕН.py
+++ Неизменяемые и изменяемые объекты. Кортежи Тетерева ЕН.py
@@ -1,7 +1,9 @@
-immutable_var = [1.0, 2, "three", True, [1, 2, 3]]
+immutable_var = 1.0, 2, "three", True, [1, 2, 3]
+print(type(immutable_var))
 print(immutable_var)
 # immutable_var[0] = 3  # TypeError: 'tuple' object does not support item assignment
 # изменить назначение элементов кортежа после того, как он создан, нельзя, потому что это его главное свойство
 mutable_list = [1.0, 2, "three", True, [1, 2, 3]]
+print(type(mutable_list))
 mutable_list[0] = 'rose'
 print(mutable_list)
